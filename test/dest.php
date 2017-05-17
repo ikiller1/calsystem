bc354ef9c2dc4e7331d611fe71629f2996b1b45c
ikiller <610218104@qq.com>
2017-05-17 11:40:13 +0800

    大量数据提交，php使用name索引数据，count计算数据个数（可变数据段仅有汇率）

--- test/dest.php
+++ test/dest.php
@@ -3,9 +3,19 @@
 <body>
 you are here<br>
 <?php
+echo "length:".count($_POST)."<br>";
 
-$pay=$_POST["pay"];
-echo "pay:".$pay;
+$length=count($_POST)-1;
+$id_body=1;
+while($length>0)
+{
+	$id="i".$id_body;
+	echo "i".$id_body.":".$_POST[$id]."<br>";
+	$length=$length-1;
+	$id_body=$id_body+1;
+}
+
+echo "pay:".$_POST["pay"]."<br>";
 
 ?> 
 </body>
