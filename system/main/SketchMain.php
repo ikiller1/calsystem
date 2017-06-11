e9dfa2b18c69de600f8418632c232c7bd1b49180
ikiller1 <610218104@qq.com>
2017-06-11 15:27:56 +0800

    1.修改editItem的参数为(row,tag,data) 2.加入新建按钮

--- system/main/SketchMain.php
+++ system/main/SketchMain.php
@@ -32,13 +32,14 @@ echo $data["_50A"]."<br>"; */
 <tr>
 <th rowspan="2">id</th>
 <th rowspan="2" colspan="2">orderid</th>
-<th colspan="3" >detail</th>
+<th colspan="4" >detail</th>
 </tr>
 	
 <tr>
 <th>date</th>
 <th>fees</th>
 <th>cars</th>
+<th>empty</th>
 </tr>
 
 
@@ -48,6 +49,15 @@ echo $data["_50A"]."<br>"; */
 <script type="text/javascript" src="main.js">
 </script>
 <script>
+var originJson;
+/* $(function() {
+
+    $( "#_50A" ).autocomplete({
+      source: "/test/EchoOrderId.php",
+      minLength: 1
+
+    });
+  }); */
 $(document).ready(function() 
 {
 	console.log("ready()");
@@ -57,9 +67,10 @@ $(document).ready(function()
 			//url:"http://www.runoob.com"
 		},
 		function(data,status){
+			originJson=data;
 			//alert("数据: \n" + data + "\n状态: " + status);
-			console.log(data.length);
-			console.log(data[0].date);
+			//console.log(data.length);
+			//console.log(data[0].date);
 			for (var i=0;i<data.length;i++)
 			{
 				console.log("row:"+i);
@@ -67,24 +78,47 @@ $(document).ready(function()
 				//addItem();
 				tables=data[i].tables;
 				addRow(i+2);
-				editItem(i+2,0,data[i].id);
-				editItem(i+2,1,data[i]._50A);
-				editItem(i+2,3,data[i].date);
+				editItem(i,"id",data[i].id);
+				editItem(i,"orderid",data[i]._50A);
+				editItem(i,"date",data[i].date);
 				for (var j=0;j<tables.length;j++)
 				{
-					console.log("col:"+j+" tablename:"+tables[j].tableName+" id:"+tables[j].id);
+					//console.log("col:"+j+" tablename:"+tables[j].tableName+" id:"+tables[j].id);
 					//document.write(cars[i] + "<br>");
 					//addItem(i,j,data[i].id,data[i].type,tables[j].tableName,tables[j].id);
-					editItem(i+2,j+4,genHref(tables[j].tableName,tables[j].id));
-					console.log(genHref(tables[j].tableName,tables[j].id));
+					editItem(i,tables[j].tag,genHref(tables[j].tableName,tables[j].id));
+					//console.log(genHref(tables[j].tableName,tables[j].id));
 				}
 			}
 			
 			//var obj = JSON.parse(data);
 			//console.log(obj[0].date);
 		});
-	//});
+	/* $("#newButton").click(function(){
+		console.log("newButton click");
+		createRecord(this);
+	}); */
 });
+function createRecord(item)
+{
+	//console.log("createRecord "+originJson);
+	console.log(item.className);
+	$.post("createRecord.php",{
+			tableName:item.className
+			//name:"菜鸟教程",
+			//url:"http://www.runoob.com"
+		},
+		function(data,status){
+			alert("数据: \n" + data + "\n状态: " + status);
+			var id=item.parentNode.parentNode.childNodes[0].innerHTML;
+			console.log(id);
+			var tableName="testtable";
+			var id_intable=17;
+			editItem(id-1,item.className,genHref(tableName,id_intable));
+			//var obj = JSON.parse(data);
+			//console.log(obj[0].date);
+		});
+}
 function addRow(row)
 {
 		console.log("addRow()");
@@ -93,6 +127,7 @@ function addRow(row)
 		var paran=document.createElement("tr");
 		{
 			var para1=document.createElement("td");
+			para1.setAttribute("class","id");
 			var node1=document.createTextNode("1");
 			para1.appendChild(node1);
 			paran.appendChild(para1);
@@ -101,7 +136,8 @@ function addRow(row)
 			var node2=document.createElement("input");
 			node2.setAttribute("type","text");
 			node2.setAttribute("required","required");
-			//node2.setAttribute("value","0");
+			node2.setAttribute("class","orderid");
+			//node2.setAttribute("id","_50A");
 			node2.setAttribute("readonly","readonly");
 			para2.appendChild(node2);
 			paran.appendChild(para2);
@@ -118,59 +154,65 @@ function addRow(row)
 			
 			var para4=document.createElement("td");
 			var node4=document.createTextNode("4");
-			
+			para4.setAttribute("class","date");
 			para4.appendChild(node4);
 			paran.appendChild(para4);
 			
 			var para5=document.createElement("td");
-			var node5=document.createElement("a");
-			node5.setAttribute("id","fees");
-			node5.setAttribute("href","#");
-			node5.innerHTML="link";
+			var node5=document.createElement("button");
+			node5.setAttribute("class","fees");
+			node5.setAttribute("onclick","createRecord(this)");
+			node5.innerHTML="new";
 			para5.appendChild(node5);
 			paran.appendChild(para5);
 			
 			var para6=document.createElement("td");
-			var node6=document.createElement("a");
-			node6.setAttribute("id","cars");
-			node6.setAttribute("href","#");
-			node6.innerHTML="link";
+			var node6=document.createElement("button");
+			node6.setAttribute("class","cars");
+			node6.setAttribute("onclick","createRecord(this)");
+			node6.innerHTML="new";
 			para6.appendChild(node6);
 			paran.appendChild(para6);
+			
+			var para7=document.createElement("td");
+			var node7=document.createElement("button");
+			node7.setAttribute("class","empty");
+			node7.setAttribute("onclick","createRecord(this)");
+			node7.innerHTML="new";
+			para7.appendChild(node7);
+			paran.appendChild(para7);
 		}
 		target.appendChild(paran);
-		console.log("child num: "+child.length);
+		//console.log("child num: "+child.length);
 }
-function editItem(row,col,data)
+function editItem(row,tag,data)
 {
 	console.log("editItem()");
-	if(col==1)
+	var child=document.getElementsByClassName(tag);
+	var target=child[row];
+	//console.log(target);
+	if(tag=="id"||tag=="date")
 	{
-		console.log("col=1 "+data);
-		var child=document.getElementsByTagName("tr");
-		//var target=child[3].parentNode;
-		var target=child[row].childNodes[col];
-		target.childNodes[0].setAttribute("value",data);
-		//target.innerHTML="test";
-		//console.log(target);
+		target.innerHTML=data;
 	}
-	else if(col>=4)
+	else if(tag=="orderid")
 	{
-		console.log("col>4:"+col);
-		var child=document.getElementsByTagName("tr");
-		//var target=child[3].parentNode;
-		var target=child[row].childNodes[col];
-		//console.log(child[row].childNodes.length);
-		//console.log(target);
-		target.childNodes[0].setAttribute("href",data);
+		target.setAttribute("value",data);
+	}
+	else if(tag=="fees"||tag=="cars"||tag=="empty")
+	{
+		console.log(child[row]);
+		var parent=target.parentNode;
+		target.setAttribute("href",data);
+		var newtarget=document.createElement("a");
+		newtarget.setAttribute("class",tag);
+		newtarget.setAttribute("href",data);
+		newtarget.innerHTML="link";
+		parent.replaceChild(newtarget,target);
 	}
 	else
 	{
-		var child=document.getElementsByTagName("tr");
-		//var target=child[3].parentNode;
-		var target=child[row].childNodes[col];
-		target.innerHTML=data;
-		//target.appendChild(para5);
+		console.log("unkown tag:"+tag);
 	}
 }
 function genHref(tableName,id)
